0cc35853539a864c43ec9dd57ae0f6244e3741b1
soltanmohammadi <msit26055@gmail.com>
2022-01-12 20:01:56 +0330

    create Note Entity

---
+++ Domain/src/main/java/com/gymix/domain/entity/Note.kt
@@ -0,0 +1,20 @@
+package com.gymix.domain.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.gymix.ui.theme.*
+
+@Entity
+data class Note(
+    val title: String,
+    val content: String,
+    val timeStamp: Long,
+    val color: Int,
+    @PrimaryKey val id: Int
+) {
+    companion object {
+        val noteColors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
+    }
+}
+
+class InvalidNoteException(message: String) : Exception(message)
