f8caee6a664e090208a3741b81683a0f5d6b2d36
soltanmohammadi <msit26055@gmail.com>
2022-01-24 20:05:07 +0330

    initialize MainActivity

--- app/src/main/java/com/gymix/training/MainActivity.kt
+++ app/src/main/java/com/gymix/training/MainActivity.kt
@@ -2,18 +2,41 @@ package com.gymix.training
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
-import com.gymix.domain.entity.Result
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.gymix.training.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-//    private val viewModel: TestViewModel by viewModels()
+    private val viewModel: MainViewModel by viewModels()
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var navHostFragment: NavHostFragment
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //create binding
+        binding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //create navHostFragment
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        //create navController
+        navController = navHostFragment.navController
+
+        //link bottom navigation to fragments
+        binding.bottomNavigation.setupWithNavController(navController)
     }
+
+
 }
