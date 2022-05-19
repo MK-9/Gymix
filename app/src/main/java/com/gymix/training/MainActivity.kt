327a4585bb30c2340055cc1851254a608382abb0
soltanmohammadi <msit26055@gmail.com>
2022-05-19 20:26:49 +0430

    use navigation in mainActivity

--- app/src/main/java/com/gymix/training/MainActivity.kt
+++ app/src/main/java/com/gymix/training/MainActivity.kt
@@ -2,18 +2,34 @@ package com.gymix.training
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.viewModels
-import com.gymix.domain.entity.Result
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.gymix.presentation.book.BookListFragment
+import com.gymix.training.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-//    private val viewModel: TestViewModel by viewModels()
+    private val navHostFragment by lazy { createNavHostFragment() }
+    private val navController: NavController by lazy { createNavController() }
+
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
+    }
+
+    private fun createNavHostFragment(): NavHostFragment {
+        return supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    }
+
+    private fun createNavController(): NavController {
+        return navHostFragment.navController
     }
 }
