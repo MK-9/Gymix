4b1af6946d3fb1e859db7ba6bb90655269719d53
soltanmohammadi <msit26055@gmail.com>
2022-06-09 21:54:06 +0430

    run example video in exo

--- Features/VideoPlayer/src/main/java/com/gymix/videoplayer/PlayerFragment.kt
+++ Features/VideoPlayer/src/main/java/com/gymix/videoplayer/PlayerFragment.kt
@@ -1,11 +1,17 @@
 package com.gymix.videoplayer
 
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSource
 import com.gymix.videoplayer.databinding.PlayerFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,13 +21,39 @@ class PlayerFragment : Fragment() {
     private val viewModel: PlayerViewModel by viewModels()
     private lateinit var binding: PlayerFragmentBinding
 
+    private var player: ExoPlayer? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = PlayerFragmentBinding.inflate(inflater)
 
+        initPlayer()
+
         return binding.root
+    }
+
+
+    private fun initPlayer() {
+        player =
+            ExoPlayer.Builder(requireContext())
+                .build()
+                .also { exoPlayer ->
+                    binding.playerView.player = exoPlayer
+                }.also { exoPlayer ->
+                    val mediaItem =
+                        MediaItem.fromUri("https://download.samplelib.com/mp4/sample-5s.mp4")
+                    exoPlayer.setMediaItem(mediaItem)
+                }.also { exoPlayer ->
+                    val mediaItem =
+                        MediaItem.fromUri("https://download.samplelib.com/mp4/sample-15s.mp4")
+                    exoPlayer.setMediaItem(mediaItem)
+                }
+
+        player?.playWhenReady = true
+
+        player?.prepare()
     }
 
     companion object {
