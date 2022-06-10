a9da789ec322832325f862a6b42ba162546ced3f
soltanmohammadi <msit26055@gmail.com>
2022-06-11 00:52:14 +0430

    add fade animation to slidingUpPanellayout

--- app/src/main/java/com/gymix/training/MainActivity.kt
+++ app/src/main/java/com/gymix/training/MainActivity.kt
@@ -2,11 +2,14 @@ package com.gymix.training
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.gymix.training.databinding.ActivityMainBinding
+import com.sothree.slidinguppanel.SlidingUpPanelLayout
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,6 +25,26 @@ class MainActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
+
+        binding.slidingLayout.addPanelSlideListener(object :
+            SlidingUpPanelLayout.PanelSlideListener {
+            override fun onPanelSlide(panel: View?, slideOffset: Float) {
+                binding.smallPlayerItem.alpha = 1f - slideOffset
+                binding.bigPlayerItem.alpha = slideOffset
+            }
+
+            override fun onPanelStateChanged(
+                panel: View?,
+                previousState: SlidingUpPanelLayout.PanelState?,
+                newState: SlidingUpPanelLayout.PanelState?
+            ) {
+                if (newState == SlidingUpPanelLayout.PanelState.COLLAPSED) {
+                    binding.bigPlayerItem.visibility = View.INVISIBLE
+                } else if (newState == SlidingUpPanelLayout.PanelState.DRAGGING) {
+                    binding.bigPlayerItem.visibility = View.VISIBLE
+                }
+            }
+        })
     }
 
     private fun createNavHostFragment(): NavHostFragment {
