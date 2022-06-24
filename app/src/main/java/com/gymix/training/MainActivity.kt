98c9490c416ae4aaffb407e103d26d1e55a56854
soltanmohammadi <msit26055@gmail.com>
2022-06-25 01:13:56 +0430

    connect activity to player service

--- app/src/main/java/com/gymix/training/MainActivity.kt
+++ app/src/main/java/com/gymix/training/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.gymix.training
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.os.IBinder
 import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import com.gymix.player.PlayerService
 import com.gymix.training.databinding.ActivityMainBinding
 import com.sothree.slidinguppanel.SlidingUpPanelLayout
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,6 +26,10 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    private var playerService: PlayerService? = null
+    private val serviceConnection: ServiceConnection by lazy { initServiceConnection() }
+    private var serviceBind = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,8 +39,8 @@ class MainActivity : AppCompatActivity() {
         binding.slidingLayout.addPanelSlideListener(object :
             SlidingUpPanelLayout.PanelSlideListener {
             override fun onPanelSlide(panel: View?, slideOffset: Float) {
-                binding.smallPlayerItem.alpha = 1f - slideOffset
-                binding.bigPlayerItem.alpha = slideOffset
+                binding.smallPlayerItem.root.alpha = 1f - slideOffset
+                binding.bigPlayerItem.root.alpha = slideOffset
             }
 
             override fun onPanelStateChanged(
@@ -39,12 +49,31 @@ class MainActivity : AppCompatActivity() {
                 newState: SlidingUpPanelLayout.PanelState?
             ) {
                 if (newState == SlidingUpPanelLayout.PanelState.COLLAPSED) {
-                    binding.bigPlayerItem.visibility = View.INVISIBLE
+                    binding.bigPlayerItem.root.visibility = View.INVISIBLE
                 } else if (newState == SlidingUpPanelLayout.PanelState.DRAGGING) {
-                    binding.bigPlayerItem.visibility = View.VISIBLE
+                    binding.bigPlayerItem.root.visibility = View.VISIBLE
                 }
             }
         })
+
+        binding.smallPlayerItem.play.setOnClickListener {
+            playAudio("https://upload.wikimedia.org/wikipedia/commons/6/6c/Grieg_Lyric_Pieces_Kobold.ogg");
+        }
+
+        binding.bigPlayerItem.play.setOnClickListener {
+            playAudio("https://upload.wikimedia.org/wikipedia/commons/6/6c/Grieg_Lyric_Pieces_Kobold.ogg");
+        }
+    }
+
+    private fun playAudio(mediaFile: String) {
+        if (!serviceBind) {
+            val intent = Intent(this, PlayerService::class.java)
+            intent.putExtra("Media", mediaFile)
+            startService(intent)
+            bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+        } else {
+
+        }
     }
 
     private fun createNavHostFragment(): NavHostFragment {
@@ -53,5 +82,20 @@ class MainActivity : AppCompatActivity() {
 
     private fun createNavController(): NavController {
         return navHostFragment.navController
+    }
+
+    private fun initServiceConnection() = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            serviceBind = true
+            playerService = (service as PlayerService.PlayerBinder).getPlayerService()
+
+            Toast.makeText(this@MainActivity, "Service Bound", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            serviceBind = false
+
+            Toast.makeText(this@MainActivity, "Service UnBound", Toast.LENGTH_SHORT).show()
+        }
     }
 }
