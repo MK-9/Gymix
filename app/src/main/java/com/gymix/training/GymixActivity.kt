487e63d2a62975de8292674e01fe061b72aa62d4
soltanmohammadi <msit26055@gmail.com>
2022-10-21 14:23:50 +0330

    add first example of compose

--- app/src/main/java/com/gymix/training/GymixActivity.kt
+++ app/src/main/java/com/gymix/training/GymixActivity.kt
@@ -3,13 +3,21 @@ package com.gymix.training
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gymix.training.ui.theme.GymixTheme
 
 class GymixActivity : ComponentActivity() {
@@ -22,22 +30,58 @@ class GymixActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
                 }
             }
         }
     }
 }
+//
+//@Composable
+//fun Greeting(name: String) {
+//    Text(text = "Hello $name!")
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    GymixTheme {
+//        Greeting("Android")
+//    }
+//}
+
+
+data class Message(val author: String, val body: String)
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    //add all padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile),
+            contentDescription = "image content Description",
+            modifier = Modifier
+                .size(size = 40.dp)
+                .clip(shape = CircleShape)
+        )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(msg.author)
+
+            // Add a vertical space between the author and message texts
+            Spacer(Modifier.height(4.dp))
+
+            Text(msg.body)
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    GymixTheme {
-        Greeting("Android")
-    }
+fun previewMessageCard() {
+    MessageCard(Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!"))
 }
