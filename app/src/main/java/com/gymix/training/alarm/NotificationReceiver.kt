d6dd91c89d8efa9467fecb727004f6be109df487
soltanmohammadi <msit26055@gmail.com>
2022-11-12 12:22:59 +0330

    create notification handler

---
+++ app/src/main/java/com/gymix/training/alarm/NotificationReceiver.kt
@@ -0,0 +1,13 @@
+package com.gymix.training.alarm
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class NotificationReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val action = intent?.action
+
+    }
+}
