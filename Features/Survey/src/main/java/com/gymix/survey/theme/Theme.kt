cc77c9bb15d511295b1800e459bad51e4faa5808
soltanmohammadi <msit26055@gmail.com>
2023-06-23 21:09:16 +0330

    create theme for light and dark

--- Features/Survey/src/main/java/com/gymix/survey/theme/Theme.kt
+++ Features/Survey/src/main/java/com/gymix/survey/theme/Theme.kt
@@ -1,4 +1,35 @@
 package com.gymix.survey.theme
 
-class Theme {
-}
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.ui.graphics.Color
+
+val LightThemeColors = lightColors(
+    primary = Purple300,
+    primaryVariant = Purple800,
+    secondary = Purple700,
+    secondaryVariant = Purple300,
+    background = Color.White,
+    surface = Color.White,
+    error = Color.Red,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onBackground = Color.White,
+    onSurface = Color.White,
+    onError = Color.Red
+)
+
+val DarkThemeColors = darkColors(
+    primary = Purple300,
+    primaryVariant = Purple800,
+    secondary = Purple700,
+    secondaryVariant = Purple300,
+    background = Color.White,
+    surface = Color.White,
+    error = Color.Red,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onBackground = Color.White,
+    onSurface = Color.White,
+    onError = Color.Red
+)
